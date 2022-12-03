ac46e354fa8c4b59f461fb5afadea78d6528229c
shashank2171 <103600766+shashank2171@users.noreply.github.com>
2022-12-03 19:09:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () =>{
     
     return(
         <>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/Groups" element={<Groups/>}/>
